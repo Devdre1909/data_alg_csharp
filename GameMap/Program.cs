4a5fb7341c65212175e5629e083134781fcee24a
Adegoke Temitope <adegoketemitope1909@gmail.com>
2021-09-28 21:45:05 +0100

    1st

---
+++ GameMap/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace GameMap
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      TerrainEnum[,] map = {
+              {TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS},
+              {TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS},
+              {TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS},
+              {TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS},
+              {TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.WALL, TerrainEnum.WALL, TerrainEnum.WALL},
+              {TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.WALL, TerrainEnum.SAND, TerrainEnum.SAND},
+              {TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.WALL, TerrainEnum.SAND, TerrainEnum.SAND},
+              {TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WALL, TerrainEnum.WALL, TerrainEnum.SAND, TerrainEnum.SAND},
+              {TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WALL, TerrainEnum.WATER, TerrainEnum.SAND, TerrainEnum.SAND},
+             {TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WALL, TerrainEnum.WALL, TerrainEnum.WATER, TerrainEnum.WATER},
+      };
+
+      // Console.OutputEncoding = UTF8Encoding.UTF8;
+
+      for (int row = 0; row < map.GetLength(0); row++)
+      {
+        for (int col = 0; col < map.GetLength(1); col++)
+        {
+          Console.ForegroundColor = map[row, col].GetColor();
+          Console.Write(map[row, col].GetChar() + " ");
+        }
+        Console.WriteLine();
+      }
+
+      Console.ForegroundColor = ConsoleColor.Gray;
+
+    }
+  }
+}
---
+++ GameMap/TerrainEnum.cs
@@ -0,0 +1,10 @@
+namespace GameMap
+{
+  public enum TerrainEnum
+  {
+    GRASS,
+    SAND,
+    WATER,
+    WALL
+  }
+}
